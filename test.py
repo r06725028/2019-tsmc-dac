ba26734dbc3d18683405e7ab7fa78d7b6e8a5f07
r06725028 <33169876+r06725028@users.noreply.github.com>
2019-12-11 15:45:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from dac_model import DAC
 
 IMG_DIM= 1200
 BATCH_SIZE = 32
-RANDOM_SEED = 1116 # set any number you like
+#RANDOM_SEED = 1116 # set any number you like
 
 def test_model(input_arr, total_size, model_dir, is_training=False):
 
@@ -42,7 +42,7 @@ def test_model(input_arr, total_size, model_dir, is_training=False):
             prob = sess.run(pred_prob, 
                 feed_dict = { inp_node: input_arr[i:j, :, :, :] })
             result_arr.append(prob)
-            print(str(prob))
+            # print(str(prob))
 
         # the last remaining part whose amout is less than BATCH_SIZE
         prob = sess.run(pred_prob,  feed_dict={ inp_node: input_arr[j:, :, :, :] })
@@ -54,17 +54,25 @@ def test_model(input_arr, total_size, model_dir, is_training=False):
         return result
 
 if __name__ == '__main__':
-        input_arr = np.load('iccad1/feature.npy').reshape(-1, IMG_DIM, IMG_DIM, 1)
-        label_arr = np.load('iccad1/label.npy').reshape(-1, 1)
+        input_arr = np.load('iccad1/test_feature.npy').reshape(-1, IMG_DIM, IMG_DIM, 1)
+        label_arr = np.load('iccad1/test_label.npy').reshape(-1, 1)
         #input_arr = np.random.rand(2, IMG_DIM, IMG_DIM, 1)
         #label_arr = np.random.randint(low=0, high=2, size=(2, 1))
         label_arr = sklearn.preprocessing.OneHotEncoder().fit_transform(label_arr).toarray()
 
-        model_dir = './checkpoint/'
+        model_dir = './checkpoints/'
         
         # get constant output
-        tf.set_random_seed(RANDOM_SEED)
+        #tf.set_random_seed(RANDOM_SEED)
 
         # * we set "is_training = False" for model parameters
         pred_distribution = test_model(input_arr, total_size=input_arr.shape[0], model_dir=model_dir, is_training=False)
+        
+        correct_prediction = tf.equal(tf.argmax(pred_distribution, 1), tf.argmax(label_arr, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # convert tensor to array
+        sess = tf.Session()
+        acc = sess.run(accuracy)
+        acc = acc.astype(np.float32)
 
+        print('accuracy : {}'.format(acc))
